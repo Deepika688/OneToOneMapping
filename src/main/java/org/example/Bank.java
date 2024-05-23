331f0197077c0b846bdd977a18fce5002719e7a8
Deepika <solankideepika7919@gmail.com>
2024-05-23 20:35:50 +0530

    onetoone commit

---
+++ src/main/java/org/example/Bank.java
@@ -0,0 +1,34 @@
+package org.example;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+    @Entity
+    public class Bank {
+
+        @Id
+        private int bid;
+        private String bName;
+        public Bank() {
+            super();
+            // TODO Auto-generated constructor stub
+        }
+        public Bank(int bid, String bName) {
+            super();
+            this.bid = bid;
+            this.bName = bName;
+        }
+        public int getBid() {
+            return bid;
+        }
+        public void setBid(int bid) {
+            this.bid = bid;
+        }
+        public String getbName() {
+            return bName;
+        }
+        public void setbName(String bName) {
+            this.bName = bName;
+        }
+
+    }
